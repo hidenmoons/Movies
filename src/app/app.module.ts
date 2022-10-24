f42af38dd825efa7defd713997d45bc2bc0a46c6
joan sistemas <alexis_dx8@hotmail.com>
2022-10-24 14:06:25 -0700

    creando menucard and tvshow page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { TvshowsComponent } from './tvshows/tvshows.component';
+import { MenucardComponent } from './menucard/menucard.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TvshowsComponent,
+    MenucardComponent
   ],
   imports: [
     BrowserModule,
