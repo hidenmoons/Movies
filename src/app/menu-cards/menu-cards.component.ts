e0d59668c33f11b1adee363d435551219fff869f
Joan Luna <alexis_dx8@hotmail.com>
2022-11-06 20:57:45 -0700

    pasando info al padre con output

--- src/app/menu-cards/menu-cards.component.ts
+++ src/app/menu-cards/menu-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Movies } from '../Models/Movies.models';
 import { ServiceService } from '../services/Movies.service';
 
